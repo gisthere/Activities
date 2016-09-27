3eb81baff98fa34e76e8acc9c0354c81d2cc4905
mr-yusupov <ziyoiddin.yusupov@gmail.com>
2016-09-27 14:13:19 +0300

    Corrected admin.py - Registered apps for admin panel

--- Activities/activity/admin.py
+++ Activities/activity/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Activity
+from .models import ActivityType
+from .models import ActivityCategory
+from .models import ActivityLocation
+from .models import Participant
+
+admin.site.register(Activity)
+admin.site.register(ActivityType)
+admin.site.register(ActivityCategory)
+admin.site.register(ActivityLocation)
+admin.site.register(Participant)
--- Activities/plans/admin.py
+++ Activities/plans/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PlannedActivity
+
+admin.site.register(PlannedActivity)
