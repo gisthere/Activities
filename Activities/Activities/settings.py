3bfb47dc871d79f4d0ee73aea9f476bf12641b48
Vladyslav Vasyliev <v.vasyliev@innopolis.ru>
2016-09-27 08:29:19 +0300

    Added apps and models for them

--- Activities/Activities/settings.py
+++ Activities/Activities/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'locations',
+    'authentication',
+    'activity',
+    'plans',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +79,17 @@ WSGI_APPLICATION = 'Activities.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  #  'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  #  }
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'activitiesdb',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
---
+++ Activities/locations/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Location(models.Model):
+	name = models.CharField(max_length=100)
+	latitude = models.CharField(max_length=7)
+	longtitude = models.CharField(max_length=7)
