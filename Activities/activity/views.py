c98bf8cf85237ef9c36f626dd118a5ff716a7d95
mr-yusupov <ziyoiddin.yusupov@gmail.com>
2016-10-04 15:36:57 +0300

    Activity creation - "urls.py" for mapping urls - "base.html" base part of template, later i will change it to bootstrap - "create.html" page for creating a new activity - "index.html" page for listing last 10 activities - "activity/views.py" controller for activity app

--- Activities/activity/views.py
+++ Activities/activity/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Activity
+from .models import ActivityCategory
+from .models import ActivityType
+
+
+def index(request):
+    activities = Activity.objects.all().order_by("-start_time")[:10]
+    context = {
+        'activities': activities
+    }
+    return render(request, 'activity/index.html', context)
+
+
+def create(request):
+    activity_categories = ActivityCategory.objects.all()
+    activity_types = ActivityType.objects.all()
+    context = {
+        'activity_categories': activity_categories,
+        'activity_types': activity_types,
+    }
+    return render(request, 'activity/create.html', context)
+
+
+def add(request):
+    name = request.POST['name']
+    description = request.POST['description']
+    requirements = request.POST['requirements']
+    participants_limit = request.POST['participants_limit']
+    activity_category_id = request.POST['activity_category']
+    activity_type_id = request.POST['activity_type']
+    activity = Activity(name=name, description=description, status='SC', requirements=requirements, participants_limit=participants_limit, activity_category=ActivityCategory.objects.get(pk=activity_category_id), activity_type=ActivityType.objects.get(pk=activity_type_id))
+    activity.save()
+    return render(request, 'activity/index.html')
