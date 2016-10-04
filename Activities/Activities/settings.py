365a87363c440470667c8c1c828425fdd75795d3
Zelios <zelios_work@outlook.com>
2016-10-04 15:14:47 +0300

    Added skeleton for navbar

--- Activities/Activities/settings.py
+++ Activities/Activities/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'authentication',
     'activity',
     'plans',
+    'templatetags'
+
 ]
 
 MIDDLEWARE = [
@@ -70,7 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+            ]
         },
     },
 ]
