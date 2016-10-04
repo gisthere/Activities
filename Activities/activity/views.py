5dfe7b370d90fa9ab6f12929595962dd8be51adf
Zelios <zelios_work@outlook.com>
2016-10-04 13:03:12 +0300

    integrated with main page, added bootstrap

--- Activities/activity/views.py
+++ Activities/activity/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.template import loader
+from django.http import HttpResponse
+from .models import Activity
 # Create your views here.
+def index(request):
+    template = loader.get_template('index.html')
+    activities = Activity.objects.all()
+    context = {
+        'activities': activities
+    }
+    return HttpResponse(template.render(context, request))
+
