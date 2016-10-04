80ae3dd020466418e3b4df3f91f37ed3529f3258
iggytoto <iggytoto@gmail.com>
2016-10-04 14:58:59 +0300

    migrations fixed

--- Activities/Activities/urls.py
+++ Activities/Activities/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin', admin.site.urls),
     url(r'^', include('authentication.urls')),
-    url(r'^$', include('activity.urls'))
+    url(r'^', include('activity.urls'))
 ]
