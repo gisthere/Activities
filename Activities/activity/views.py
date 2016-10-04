aec69df7a5f5b94a5034ab27fcb935ecf0ed4ebf
Zelios <zelios_work@outlook.com>
2016-10-04 13:36:51 +0300

    added filter by activity type

--- Activities/activity/views.py
+++ Activities/activity/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from .models import Activity
+from .models import Activity, ActivityType, ActivityCategory
 # Create your views here.
 def index(request):
+    types = ActivityType.objects.order_by('id')[:10]
+    categories = ActivityCategory.objects.order_by('id')[:10]
     template = loader.get_template('index.html')
-    activities = Activity.objects.all()
+    if 'activity_type' in request.GET and request.GET['activity_type'].strip():
+        query = request.GET['activity_type']
+
+        activities = Activity.objects.filter(activity_type=query)
+    else:
+        activities = Activity.objects.all()
     context = {
-        'activities': activities
+        'activities': activities,
+        'types': types,
+        'categories': categories
     }
     return HttpResponse(template.render(context, request))
 
