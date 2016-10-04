8456c475ea09b5429406ddfba630e363f1bf5ff1
iggytoto <iggytoto@gmail.com>
2016-10-04 17:43:28 +0300

    fix

--- Activities/activity/urls.py
+++ Activities/activity/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'activity'
 urlpatterns = [
-    url(r'^activity/', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'activity/create/', views.create, name='create_activity'),
     url(r'activity/add/', views.add, name='add_activity')
 ]
