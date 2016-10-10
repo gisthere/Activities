667e97049c3f02ae9baf61b9eecb9c96c546f329
Zelios <zelios_work@outlook.com>
2016-10-11 00:09:21 +0300

    Add autocomplete widget

--- Activities/activity/views.py
+++ Activities/activity/views.py
@@ -1,24 +1,31 @@
+import json
 from django.shortcuts import render
-
+from django.core import serializers
 from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import Activity, ActivityType, ActivityCategory
-# Create your views here.
-def index(request):
-    types = ActivityType.objects.order_by('id')[:10]
-    categories = ActivityCategory.objects.order_by('id')[:10]
-    template = loader.get_template('index.html')
-    if 'activity_type' in request.GET and request.GET['activity_type'].strip():
-        query = request.GET['activity_type']
 
-        activities = Activity.objects.filter(activity_type=query)
+def index(request):
+    # get search 't' - type and 'v' value 
+    if 't' in request.GET and 'v' in request.GET:
+        filter_type = request.GET['t']
+        filter_value = request.GET['v']
+
+        # check possible variation to prevent XSS injection
+        if filter_type == 'at': 
+            result = Activity.objects.filter(activity_type = filter_value)
+        elif filter_type == 'ac':
+            result = Activity.objects.filter(activity_category = filter_value)
+        else:
+            result = Activity.objects.filter(name__istartswith = filter_value)
     else:
-        activities = Activity.objects.all()
+        result = Activity.objects
+
     context = {
-        'activities': activities,
-        'types': types,
-        'categories': categories
+        'activities': result.all()
     }
+
+    template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
 
 
@@ -43,3 +50,35 @@ def add(request):
     activity.save()
     #temp solution, should show my activities
     return HttpResponseRedirect('/')
+
+
+def get_search_suggestions(request):
+    if 'search' not in request.GET:
+        return JsonResponse({})
+
+    query_parameter = request.GET['search']
+
+    result = {}
+    types_query = ActivityType.objects.filter(
+        name__istartswith=query_parameter
+    )
+
+    result = {}
+    if types_query.count() < 2:
+        categories_query = ActivityCategory.objects.filter(
+            name__istartswith=query_parameter
+        )
+        data = categories_query
+
+        if categories_query.count() != 0:
+            result['types'] = 'ac'
+        else:
+            result['types'] = 'm'
+    else:
+        data = types_query
+        result['types'] = 'at'
+
+    result['data'] = list(data.values())
+    qs_json = json.dumps(result),
+
+    return HttpResponse(qs_json, content_type='application/json')
