7bb5f5c75a08770bc2921f1bdafb7bcdb2398cf0
Vladyslav Vasyliev <v.vasyliev@innopolis.ru>
2016-10-18 15:55:13 +0300

    Added possibility to dismiss participation in the chosen activity.

--- Activities/activity/views.py
+++ Activities/activity/views.py
@@ -1,6 +1,8 @@
 import json
 import random
 from itertools import chain
+
+from django.contrib.auth.models import User
 from django.db.models import Model
 from django.shortcuts import render
 from django.http import Http404
@@ -14,7 +16,6 @@ from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models import Value, CharField, FloatField
 
 
-
 # Create your views here.
 def index(request):
     # get search 't' - type and 'v' value 
@@ -23,14 +24,14 @@ def index(request):
         filter_value = request.GET['v']
 
         # check possible variation to prevent XSS injection
-        if filter_type == 'at': 
-            result = Activity.objects.filter(activity_type = filter_value)
+        if filter_type == 'at':
+            result = Activity.objects.filter(activity_type=filter_value)
         elif filter_type == 'ac':
-            result = Activity.objects.filter(activity_category = filter_value)
+            result = Activity.objects.filter(activity_category=filter_value)
         elif filter_type == 'an':
             result = Activity.objects.annotate(
-                similarity = TrigramSimilarity('name', request.GET['search'])
-                ).filter(similarity__gt=0.1)
+                similarity=TrigramSimilarity('name', request.GET['search'])
+            ).filter(similarity__gt=0.1)
     else:
         result = Activity.objects
 
@@ -49,29 +50,28 @@ def get_hints(request):
     search = request.GET['q']
     # name__istartswith
     activity_data = Activity.objects.annotate(
-        similarity = TrigramSimilarity('name', search),
-        f_name = Value('an', output_field=CharField())
+        similarity=TrigramSimilarity('name', search),
+        f_name=Value('an', output_field=CharField())
     ).filter(similarity__gt=0.1).values('id', 'name', 'similarity', 'f_name')
 
     activity_type_data = ActivityType.objects.annotate(
-        similarity = Value(0.8, output_field=FloatField()),
-        f_name = Value('at', output_field=CharField())
+        similarity=Value(0.8, output_field=FloatField()),
+        f_name=Value('at', output_field=CharField())
     ).filter(name__istartswith=search).values('id', 'name', 'similarity', 'f_name')
 
     activity_category_data = ActivityCategory.objects.annotate(
-        similarity =  Value(0.6, output_field=FloatField()),
-        f_name = Value('ac', output_field=CharField())
+        similarity=Value(0.6, output_field=FloatField()),
+        f_name=Value('ac', output_field=CharField())
     ).filter(name__istartswith=search).values('id', 'name', 'similarity', 'f_name')
 
     # Get top most suitable hint by order by similarity
     hints = sorted(chain(activity_data, activity_type_data, activity_category_data),
-        key=lambda x: x['similarity'], reverse=True)
+                   key=lambda x: x['similarity'], reverse=True)
 
     result = list(hints)
 
     result_json = json.dumps(result)
     return HttpResponse(result_json, content_type='application/json')
-
 
 
 def calcAvailableSpots(activities):
@@ -96,13 +96,13 @@ def create(request):
         return render(request, 'activity/create.html', {'activity_form': form})
     else:
         return HttpResponseRedirect('login/')
-    # activity_categories = ActivityCategory.objects.all()
-    # activity_types = ActivityType.objects.all()
-    # context = {
-    #     'activity_categories': activity_categories,
-    #     'activity_types': activity_types,
-    # }
-    # return render(request, 'activity/create.html', context)
+        # activity_categories = ActivityCategory.objects.all()
+        # activity_types = ActivityType.objects.all()
+        # context = {
+        #     'activity_categories': activity_categories,
+        #     'activity_types': activity_types,
+        # }
+        # return render(request, 'activity/create.html', context)
 
 
 @register.filter
@@ -110,8 +110,8 @@ def get_item(dictionary, key):
     return dictionary.get(key)
 
 
+# Delete activity by specified id (passed as get parameter)
 def delete(request):
-    print('delete hello activity_id')
     # redirect to main page if the user is not authenticated
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/')
@@ -124,6 +124,27 @@ def delete(request):
         if activity.organizer.id != request.user.id:
             return HttpResponse()
         activity.delete()
+    except Model.DoesNotExist as e:
+        print(e)
+    return HttpResponse()
+
+
+# Dismiss activity by specified activity id and user id (passed as get parameters)
+def dismiss(request):
+    # redirect to main page if the user is not authenticated
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/')
+    activity_id = request.GET.get('activity_id')
+    user_id = request.GET.get('user_id')
+    # do nothing if there is no valid info in request
+    if activity_id is None or user_id is None:
+        return HttpResponse()
+    try:
+        # try to load activity from the database
+        activity = Activity.objects.get(id=activity_id)
+        user = User.objects.get(id=user_id)
+        part_activity = Participant.objects.get(user=user, activity=activity)
+        part_activity.delete()
     except Model.DoesNotExist as e:
         print(e)
     return HttpResponse()
