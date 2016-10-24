022d9316382ded1e6745ec50aaeee8ad4bb92167
Vladyslav Vasyliev <v.vasyliev@innopolis.ru>
2016-10-24 03:31:51 +0300

    Implemented control over the states of the activities. Added basic ability to rate activities.

--- Activities/activity/urls.py
+++ Activities/activity/urls.py
@@ -9,8 +9,11 @@ urlpatterns = [
     url(r'^activity/(?P<activity_id>\d+)/$', views.detail, name='activity_detail'),
     url(r'^activity/(?P<pk>\d+)/edit$', views.detail, name='activity_edit'),
     url(r'^activity/delete', views.delete, name='activity_delete'),
+
     url(r'activity/hints/', views.get_hints, name='activity_hints'),
+
     url(r'activity/dismiss', views.dismiss, name='dismiss_activity'),
     url(r'activity/join', views.join, name='join_activity'),
-
+    url(r'activity/status', views.change_status, name='change_status'),
+    url(r'activity/rating', views.change_rating, name='change_rating'),
 ]
