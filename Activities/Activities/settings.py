90096765326162f7fbbbee2cbc19b08a4c122182
Vladyslav Vasyliev <v.vasyliev@innopolis.ru>
2016-11-01 05:03:33 +0300

    Added basic support of the map to the activity page.

--- Activities/Activities/settings.py
+++ Activities/Activities/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
+    'leaflet',
     'locations',
     'authentication',
     'activity',
@@ -127,6 +128,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LEAFLET_CONFIG = {
+  'DEFAULT_CENTER': (00.00, 00.00),
+  'DEFAULT_ZOOM': 1,
+  'MIN_ZOOM': 1,
+  'MAX_ZOOM': 20,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
