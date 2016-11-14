a3bc64c866055da5c5ba725382aee1f9478a38a2
Vladyslav Vasyliev <v.vasyliev@innopolis.ru>
2016-11-14 23:56:42 +0300

    Fixed validation and added datepicker for Birth date

--- Activities/cabinet/forms.py
+++ Activities/cabinet/forms.py
@@ -22,7 +22,7 @@ def validate_telegram(telegram):
 
 def validate_date(date):
     if not re.match(r'^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$', date):
-        raise ValidationError('Incorrect Date')
+        raise ValidationError('Incorrect Date. It should be in format YYYY-MM-DD')
 
 
 def validate_gender(gender):
@@ -36,13 +36,20 @@ class UserSettingsForm(forms.Form):
     GENDER = ((MALE, 'Male'), (FEMALE, 'Female'))
 
     title = 'User profile'
-    first_name = forms.CharField(label='First name', max_length=30, required=False)
-    last_name = forms.CharField(label='Last name', max_length=30, required=False)
-    email = forms.EmailField(label='Email', max_length=254, required=False, validators=[validator_email])
-    phone = forms.CharField(label='Phone', max_length=11, required=False, validators=[validate_phone])
-    telegram = forms.CharField(label='Telegram', max_length=30, required=False, validators=[validate_telegram])
-    birth_date = forms.CharField(label='Birth date', max_length=30, required=False, validators=[validate_date])
-    gender = forms.ChoiceField(choices=GENDER, required=False, widget=forms.Select(attrs={'class' : 'form-control'}))
+    first_name = forms.CharField(label='First name', max_length=30, required=False,
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First name'}))
+    last_name = forms.CharField(label='Last name', max_length=30, required=False,
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last name'}))
+    email = forms.EmailField(label='Email', max_length=254, required=False, validators=[validator_email],
+                             widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    phone = forms.CharField(label='Phone', max_length=11, required=False, validators=[validate_phone],
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}))
+    telegram = forms.CharField(label='Telegram', max_length=30, required=False, validators=[validate_telegram],
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Telegram'}))
+    birth_date = forms.CharField(label='Birth date', max_length=10, required=False, validators=[validate_date],
+                                 widget=forms.TextInput(attrs={'class': 'form-control span2', 'placeholder': 'Birth date'}))
+    gender = forms.ChoiceField(choices=GENDER, required=False,
+                               widget=forms.Select(attrs={'class': 'form-control', 'placeholder': 'Gender'}))
 
     class Meta:
         fields = ['first_name', 'last_name', 'email', 'phone', 'telegram', 'birth_date', 'gender']
